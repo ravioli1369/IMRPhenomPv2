5624a683fff17b8dceefc6e205d54c647300dc82
Ravi Kumar <ravioli1369@gmail.com>
2024-12-29 02:13:28 +0000

    switch to sampling bilby parameters and converting to lal

--- run_phenomp.py
+++ run_phenomp.py
@@ -1,78 +1,96 @@
 import lal
+import lalsimulation
+from astropy import units as u
 import torch
 import numpy as np
 import jax.numpy as jnp
 from torch import Tensor
 from phenom_p import IMRPhenomPv2
 from IMRPhenomPv2 import gen_IMRPhenomPv2
+from conversion import bilby_spins_to_lalsim, chirp_mass_and_mass_ratio_to_components
 
 
-def ms_to_Mc_eta(m):
-    r"""
-    Converts binary component masses to chirp mass and symmetric mass ratio.
+chirp_mass = torch.tensor([30.0])
+mass_ratio = torch.tensor([0.8])
+mass_1, mass_2 = chirp_mass_and_mass_ratio_to_components(chirp_mass, mass_ratio)
+f_ref = 20.0
+sample_rate = 2048.0
 
-    Args:
-        m: the binary component masses ``(m1, m2)``
+a_1 = torch.tensor([0.5])
+a_2 = torch.tensor([0.8])
+tilt_1 = torch.tensor([np.pi / 3])
+tilt_2 = torch.tensor([np.pi / 2])
+phi_12 = torch.tensor([np.pi / 4])
+phi_jl = torch.tensor([np.pi / 6])
+distance = torch.tensor([500.0])
+theta_jn = torch.tensor([np.pi / 4])
+phase = torch.tensor([7 * np.pi / 4])
 
-    Returns:
-        :math:`(\mathcal{M}, \eta)`, with the chirp mass in the same units as
-        the component masses
-    """
-    m1, m2 = m
-    return (m1 * m2) ** (3 / 5) / (m1 + m2) ** (1 / 5), m1 * m2 / (m1 + m2) ** 2
+inclination, chi1x, chi1y, chi1z, chi2x, chi2y, chi2z = bilby_spins_to_lalsim(
+    theta_jn,
+    phi_jl,
+    tilt_1,
+    tilt_2,
+    phi_12,
+    a_1,
+    a_2,
+    mass_1,
+    mass_2,
+    f_ref,
+    phase,
+)
+
+params = dict(
+    m1=mass_1.item() * lal.MSUN_SI,
+    m2=mass_2.item() * lal.MSUN_SI,
+    S1x=chi1x.item(),
+    S1y=chi1y.item(),
+    S1z=chi1z.item(),
+    S2x=chi2x.item(),
+    S2y=chi2y.item(),
+    S2z=chi2z.item(),
+    distance=(distance.item() * u.Mpc).to("m").value,
+    inclination=inclination.item(),
+    phiRef=phase.item(),
+    longAscNodes=0.0,
+    eccentricity=0.0,
+    meanPerAno=0.0,
+    deltaF=1.0 / sample_rate,
+    f_min=10.0,
+    f_ref=f_ref,
+    f_max=300,
+    approximant=lalsimulation.IMRPhenomPv2,
+    LALpars=lal.CreateDict(),
+)
+hp_lal, hc_lal = lalsimulation.SimInspiralChooseFDWaveform(**params)
 
 
-# Frequency grid
-T = 128
-f_l = 20.0
-f_sampling = 4096
-f_u = f_sampling // 2
-f_ref = f_l
+lal_freqs = np.array(
+    [hp_lal.f0 + ii * hp_lal.deltaF for ii in range(len(hp_lal.data.data))]
+)
 
-delta_t = 1 / f_sampling
-tlen = int(round(T / delta_t))
-freqs = np.fft.rfftfreq(tlen, delta_t)
-df = freqs[1] - freqs[0]
-fs = freqs[(freqs > f_l) & (freqs < f_u)]
-
-
-m1_msun = Tensor([40])
-m2_msun = Tensor([25])
-chirp_mass = Tensor([(m1_msun * m2_msun) ** (3 / 5) / (m1_msun + m2_msun) ** (1 / 5)])
-mass_ratio = Tensor([m1_msun / m2_msun])
-m1_kg = Tensor([m1_msun * lal.MSUN_SI])
-m2_kg = Tensor([m2_msun * lal.MSUN_SI])
-chi1z = Tensor([0.1])
-chi2z = Tensor([-0.1])
-chi1x = Tensor([-0.2])
-chi2x = Tensor([0.2])
-chi1y = Tensor([0.9])
-chi2y = Tensor([-0.4])
-dist_mpc = Tensor([100])
-distance = Tensor([dist_mpc * 1e6 * lal.PC_SI])
-tc = Tensor([0])
-phic = Tensor([torch.pi])
-inclination = Tensor([0])
-Mc, eta = ms_to_Mc_eta(jnp.array([m1_msun.item(), m2_msun.item()]))
 theta_ripple = jnp.array(
     [
-        m1_msun.item(),
-        m2_msun.item(),
+        mass_1.item(),
+        mass_2.item(),
         chi1x.item(),
         chi1y.item(),
         chi1z.item(),
         chi2x.item(),
         chi2y.item(),
         chi2z.item(),
-        dist_mpc.item(),
-        tc.item(),
-        phic.item(),
+        distance.item(),
+        0,
+        phase.item(),
         inclination.item(),
     ]
 )
-fs_ripple = jnp.arange(f_l, f_u, df)[1:]
-fs_torch = Tensor(np.arange(f_l, f_u, df)[1:])
 
+lal_mask = (lal_freqs > params["f_min"]) & (lal_freqs < params["f_max"])
+
+lal_freqs = lal_freqs[lal_mask]
+ml4gw_freqs = torch.tensor(lal_freqs, dtype=torch.float32)
+ripple_freqs = jnp.array(lal_freqs)
 
 import argparse
 
@@ -81,8 +99,8 @@ parser.add_argument("--torch", action="store_true")
 args = parser.parse_args()
 
 if args.torch:
-    hp_torch, hc_torch = IMRPhenomPv2().forward(
-        fs_torch,
+    hc_ml4gw, hp_ml4gw = IMRPhenomPv2().forward(
+        ml4gw_freqs,
         chirp_mass,
         mass_ratio,
         chi1x,
@@ -91,11 +109,11 @@ if args.torch:
         chi2x,
         chi2y,
         chi2z,
-        dist_mpc,
-        phic,
+        distance,
+        phase,
         inclination,
         f_ref,
-        tc,
+        torch.tensor([0.0]),
     )
 else:
-    hp_ripple, hc_ripple = gen_IMRPhenomPv2(fs_ripple, theta_ripple, f_ref)
+    hp_ripple, hc_ripple = gen_IMRPhenomPv2(ripple_freqs, theta_ripple, f_ref)
